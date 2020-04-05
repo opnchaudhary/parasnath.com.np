656a4b128a80421fdc92fed91ae56b07669c53e4
Paras Nath Chaudhary <opnchaudhary@gmail.com>
2020-04-05 21:14:09 +0545

    new website in progress

---
+++ src/public/demos/acas/assets/js/app.js
@@ -0,0 +1,64 @@
+function getPrograms(){
+    $.get("/ajax/programs.html", function(data, status){
+        $('#main-content').html(data);
+    });
+    // $.get("/ajax/programs-left-panel.html");
+}
+function getPad(){
+    $.get("/ajax/pad.html", function(data, status){
+        $('#main-content').html(data);
+    });
+    $.get("/ajax/pad-left-panel.html", function(data, status){
+        $('#left-dynamic-panel').html(data);
+    });
+}
+function getLive(){
+    $.get("/ajax/live.html", function(data, status){
+        $('#main-content').html(data);
+    });
+    $.get("/ajax/live-left-panel.html", function(data, status){
+        $('#left-dynamic-panel').html(data);
+    });
+}
+$(document).ready(function(){
+    getPrograms();
+    $('#programs-menu').click(function(){
+        getPrograms();
+    })
+    $('#pad-menu').click(function(){
+        getPad();
+    })
+    $('#live-menu').click(function(){
+        getLive();
+    });
+
+    $('#open-left-panel').click(function() {
+        console.log('open left panel');
+        var leftPanel = $("#left-panel");
+        leftPanel.removeClass('d-none');
+    });
+    $('#open-right-panel').click(function() {
+        console.log('open right panel');
+        var rightPanel = $("#right-panel");
+        rightPanel.removeClass('d-none');
+    });
+    $('#close-left-panel').click(function(){
+        $('#left-panel').addClass('d-none');
+    });
+    $('#close-right-panel').click(function(){
+        $('#right-panel').addClass('d-none');
+    });
+
+
+    $(".collapse.show").each(function(){
+        $(this).prev("h2").find(".fa").addClass("fa-play").removeClass("fa-sort-down");
+    });
+
+    // Toggle plus minus icon on show hide of collapse element
+    $(".collapse").on('show.bs.collapse', function(){
+        $(this).prev("h2").find(".fa").removeClass("fa-play").addClass("fa-sort-down");
+    }).on('hide.bs.collapse', function(){
+        $(this).prev("h2").find(".fa").removeClass("fa-sort-down").addClass("fa-play");
+    });
+
+});
